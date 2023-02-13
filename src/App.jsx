3b156da51f513f84e592309debfc044b79e6ff6d
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-02-13 03:35:30 -0700

    adjusting home route

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/moival/' element={<Home />} />
+          <Route path='/' element={<Home />} />
           <Route path='/projects' element={<Projects />} />
           <Route path='/project/:id' element={<ProjectDisplay />} />
           <Route path='/experience' element={<Experience />} />
