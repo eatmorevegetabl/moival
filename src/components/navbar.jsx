bf53b945f5f6ae0bffb2148dcb43424f2dfa5d95
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-02-13 03:39:52 -0700

    adjusting home route

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -31,7 +31,7 @@ export default function Navbar() {
     <nav className='bg-teal-500 p-6 lg:flex lg:justify-between'>
       <div className='flex items-center justify-between flex-wrap'>
         <div className='flex items-center flex-shrink-0 text-white mr-6'>
-          <Link to='/' className='flex'>
+          <Link to='/moival' className='flex'>
             <MdComputer className='fill-current mr-2 text-3xl' />
             <span className='font-semibold text-xl tracking-tight'>
               Moisés Valerin
@@ -60,7 +60,7 @@ export default function Navbar() {
         <div className='w-full flex-grow lg:flex lg:items-center lg:w-auto'>
           <div className='text-lg lg:flex-grow'>
             <Link
-              to='/'
+              to='/moival'
               className='block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4'
             >
               Home
