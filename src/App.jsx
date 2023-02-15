25ea187191208c4f931d96e463b6c72036557360
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-02-14 17:47:58 -0700

    fix for Route exact path to home screen

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
+          <Route exact path='/moival' element={<Home />} />
           <Route path='/projects' element={<Projects />} />
           <Route path='/project/:id' element={<ProjectDisplay />} />
           <Route path='/experience' element={<Experience />} />
